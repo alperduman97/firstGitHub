6f005ce5faa5342996a6b78fda42764f17662d2d
alperduman <alprdmn1907@gmail.com>
2023-12-09 19:07:55 +0100

    2.Gün ilk version

--- src/Second.java
+++ src/Second.java
@@ -1,5 +1,8 @@
 public class Second {
     public static void main(String[] args) {
+        int a = 128;
+        int b=256;
+        System.out.println("İki sayının toplami:" + (a+b));
 
     }
 }
