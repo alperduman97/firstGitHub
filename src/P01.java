61eda708304aee6b3dc10797ab3836919c1e4dac
alperduman <alprdmn1907@gmail.com>
2023-12-09 20:17:44 +0100

    asd

--- src/P01.java
+++ src/P01.java
@@ -1,2 +1,6 @@
 public class P01 {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba v 2.5");
+    }
 }
--- src/Second.java
+++ src/Second.java
@@ -1,8 +1,13 @@
 public class Second {
     public static void main(String[] args) {
         int a = 128;
-        int b=256;
-        System.out.println("İki sayının toplami:" + (a+b));
+        int b = 256;
+        System.out.println("İki sayının toplami:" + (a + b));
 
     }
+
+    //git add .
+    //git commmit -m "...";
+    //git push
+//
 }
